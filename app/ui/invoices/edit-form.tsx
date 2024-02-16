734ad9f919bd19f58611bc45ad4470f731224f45
migramirez2 <tusharvakhare9898@gmail.com>
2024-02-16 22:55:58 +0900

    fix: undefined component

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -39,7 +39,7 @@ export default function EditInvoiceForm({
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
                 </option>
-              ))} 
+              ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
